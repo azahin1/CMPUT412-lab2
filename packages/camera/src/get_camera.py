c74a7f3ad8316c3a972f5cdb1b5d71d112e9c881
azahin1 <azahin1@ualberta.ca>
2023-02-08 14:42:29 -0700

    tried working the image, moving over to wheels

--- packages/camera/src/get_camera.py
+++ packages/camera/src/get_camera.py
@@ -11,12 +11,11 @@ class MySubscriberNode(DTROS):
         # initialize the DTROS parent class
         super(MySubscriberNode, self).__init__(node_name=node_name, node_type=NodeType.GENERIC)
         # construct publisher
-        # botName = os.environ['VEHICLE_NAME']
-        self.sub = rospy.Subscriber('csc22946/camera_node/image/compressed', CompressedImage, self.callback)
+        botName = os.environ['HOSTNAME']
+        self.sub = rospy.Subscriber(f"{botName}/camera_node/image/compressed", CompressedImage, self.callback)
 
     def callback(self, data):
-        print(getattr(data))
-        rospy.loginfo(f"Image size: lmao")
+        rospy.loginfo(f"Image size: {data.format}")
 
 if __name__ == '__main__':
     # create the node
